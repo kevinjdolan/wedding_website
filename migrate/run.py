73646fbae8700bfd9ee2b06f33c663b41caf5497
Kevin Dolan <norelaxation@gmail.com>
2020-01-14 15:59:09 -0800

    use actual migration now

--- migrate/run.py
+++ migrate/run.py
@@ -1,6 +1,5 @@
-# from alembic.config import Config
-# from alembic import command
-#
-# alembic_cfg = Config("./alembic.ini")
-# command.upgrade(alembic_cfg, "head")
-raise
+from alembic.config import Config
+from alembic import command
+
+alembic_cfg = Config("./alembic.ini")
+command.upgrade(alembic_cfg, "head")
